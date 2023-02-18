af1cbde69e3417427c2c76eb4a9aea8e7bbef80b
Max <m.fadeev@geniusee.com>
2023-02-18 13:10:35 +0200

    add mutex to fix race condition

--- cache.go
+++ cache.go
@@ -2,26 +2,35 @@ package cache
 
 import (
 	"errors"
+	"sync"
 	"time"
 )
 
 type Cache struct {
 	cache            map[string]interface{}
 	keyDeleteTimeMap map[string]time.Time
+	mutex            sync.RWMutex
 }
 
-func New() Cache {
-	cache := Cache{make(map[string]interface{}), make(map[string]time.Time)}
+func New() *Cache {
+	cache := Cache{
+		cache:            make(map[string]interface{}),
+		keyDeleteTimeMap: make(map[string]time.Time),
+	}
 	go cache.deletingExpiredCacheWorker()
-	return cache
+	return &cache
 }
 
 func (c *Cache) Set(key string, value interface{}, duration time.Duration) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	c.cache[key] = value
 	c.keyDeleteTimeMap[key] = time.Now().Add(duration)
 }
 
 func (c *Cache) Get(key string) (interface{}, error) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
 	value, ok := c.cache[key]
 	if !ok {
 		return nil, errors.New("key doesn't exists")
