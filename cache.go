18c72124ba8d43c7dccb4338deaf0919d8bee9e9
Max <m.fadeev@geniusee.com>
2023-02-18 12:45:38 +0200

    add cache duration

--- cache.go
+++ cache.go
@@ -1,17 +1,24 @@
 package cache
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 type Cache struct {
-	cache map[string]interface{}
+	cache            map[string]interface{}
+	keyDeleteTimeMap map[string]time.Time
 }
 
 func New() Cache {
-	return Cache{make(map[string]interface{})}
+	cache := Cache{make(map[string]interface{}), make(map[string]time.Time)}
+	cache.deletingExpiredCacheWorker()
+	return cache
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key string, value interface{}, duration time.Duration) {
 	c.cache[key] = value
+	c.keyDeleteTimeMap[key] = time.Now().Add(duration)
 }
 
 func (c *Cache) Get(key string) (interface{}, error) {
@@ -20,6 +27,17 @@ func (c *Cache) Get(key string) (interface{}, error) {
 		return nil, errors.New("key doesn't exists")
 	}
 	return value, nil
+}
+
+func (c *Cache) deletingExpiredCacheWorker() {
+	for {
+		for key, expiryTime := range c.keyDeleteTimeMap {
+			if expiryTime.Before(time.Now()) {
+				delete(c.cache, key)
+				delete(c.keyDeleteTimeMap, key)
+			}
+		}
+	}
 }
 
 func (c *Cache) Delete(key string) error {
